ba8eb8786052f76592867a501b8abac810920ecd
Woze Parrot <wozeparrot@gmail.com>
2023-05-21 18:33:48 -0400

    feat: update to zig master

--- src/main.zig
+++ src/main.zig
@@ -58,8 +58,7 @@ pub fn main() !void {
     defer config_file.close();
     var buf: [2 * 1024 * 1024]u8 = undefined;
     const len = try config_file.readAll(buf[0..]);
-    var token_stream = json.TokenStream.init(buf[0..len]);
-    var state = try json.parse(State, &token_stream, .{ .allocator = alloc });
+    var state = try json.parseFromSlice(State, alloc, buf[0..len], .{});
     var state_lock = std.Thread.RwLock{};
 
     // load gossip key
